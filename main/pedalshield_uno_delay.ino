f0b0b685d5eb3faee5adc11c9badce45f020c420
avaevelyn <ava.horwood79@schools.sa.edu.au>
2023-03-12 15:45:42 +1030

    button check, commenting,

--- main/pedalshield_uno_delay.ino
+++ main/pedalshield_uno_delay.ino
@@ -1,10 +1,8 @@
 // CC-by-www.Electrosmash.com
 // Based on OpenMusicLabs previous works.
-// pedalshield_uno_fuzz.ino reads the ADC signal and boost it to the max or min value depending on a threshold value
-// pressing the pushbutton_1 or 2 turns the fuzz distortion harder or softer.
-// Made by Ava Horwood
- 
- 
+// pedalshield_uno_delay.ino reads the ADC, saves it into the DelayBuffer[] array and plays it into the PWM output.
+//  pressing the pushbutton_1 or 2 makes the delay longer or shorter
+
 //defining harware resources.
 #define LED 13
 #define FOOTSWITCH 12
@@ -18,26 +16,29 @@
 #define PWM_QTY 2 // 2 PWMs in parallel
 
 //other variables
-int input, distortion_threshold=6000; //initial value adjusted by try and error.
+int input, vol_variable=512;
 int counter=0;
 unsigned int ADC_low, ADC_high;
- 
+
+#define MAX_DELAY 2000
+byte DelayBuffer[MAX_DELAY];
+unsigned int DelayCounter = 0;
+unsigned int Delay_Depth = MAX_DELAY;
+
 void setup() {
- 
   //setup IO
   pinMode(FOOTSWITCH, INPUT_PULLUP);
   pinMode(TOGGLE, INPUT_PULLUP);
   pinMode(PUSHBUTTON_1, INPUT_PULLUP);
   pinMode(PUSHBUTTON_2, INPUT_PULLUP);
   pinMode(LED, OUTPUT);
-  Serial.begin(9600);
- 
+  
   // setup ADC
   ADMUX = 0x60; // left adjust, adc0, internal vcc
   ADCSRA = 0xe5; // turn on adc, ck/32, auto trigger
   ADCSRB = 0x07; // t1 capture for trigger
   DIDR0 = 0x01; // turn off digital inputs for adc0
- 
+
   // setup PWM
   TCCR1A = (((PWM_QTY - 1) << 5) | 0x80 | (PWM_MODE << 1)); //
   TCCR1B = ((PWM_MODE << 3) | 0x11); // ck/1
@@ -47,52 +48,47 @@ void setup() {
   DDRB |= ((PWM_QTY << 1) | 0x02); // turn on outputs
   sei(); // turn on interrupts - not really necessary with arduino
   }
- 
+
 void loop() 
 {
     //Turn on the LED if the effect is ON.
-  if (digitalRead(FOOTSWITCH)){
-    digitalWrite(LED, HIGH); 
-//    Serial.println("effect is on");
-    } 
-     else {
-      digitalWrite(LED, LOW);
-//      Serial.println("effect is off");
-     }
+  if (digitalRead(FOOTSWITCH)) digitalWrite(LED, HIGH); 
+     else  digitalWrite(LED, LOW);
   //nothing here, all happens in the Timer 1 interruption.
 }
- 
+
 ISR(TIMER1_CAPT_vect) 
 {
   // get ADC data
-  ADC_low = ADCL; // you need to fetch the low byte first
+  ADC_low = 0; // ADC_low always 0 to save space
   ADC_high = ADCH;
-  //construct the input sumple summing the ADC low and high byte.
-  input = ((ADC_high << 8) | ADC_low) + 0x8000; // make a signed 16b value
+  
+  //store the high bit only for 
+  DelayBuffer[DelayCounter] = ADC_high;
  
-counter++; //to save resources, the pushbuttons are checked every 1000 times.
-if(counter==1000)
+counter++; //to save resources, the pushbuttons are checked every 100 times.
+if(counter==100)
 { 
 counter=0;
-if (!digitalRead(PUSHBUTTON_2)) {
-  Serial.println("Increasing Volume");
-  if (distortion_threshold<32768)distortion_threshold=distortion_threshold+25; //increase the vol
+if (!digitalRead(PUSHBUTTON_1)) {
+  if (Delay_Depth<MAX_DELAY)Delay_Depth=Delay_Depth+1; //increase the vol
     digitalWrite(LED, LOW); //blinks the led
     }
- 
-    if (!digitalRead(PUSHBUTTON_1)) {
-        Serial.println("Decreasing Volume");
-  if (distortion_threshold>0)distortion_threshold=distortion_threshold-25; //decrease vol
+
+    if (!digitalRead(PUSHBUTTON_2)) {
+  if (Delay_Depth>0)Delay_Depth=Delay_Depth-1; //decrease vol
   digitalWrite(LED, LOW); //blinks the led
     }
 }
- 
-  //the input signal is 16bits (values from -32768 to +32768
-  //the value is clipped to the distortion_threshold value
-    if(input>distortion_threshold) input=32768;
-    else if(input<-distortion_threshold) input=-32768;
- 
+
+//Increse/reset delay counter.   
+  DelayCounter++;
+  if(DelayCounter >= Delay_Depth) DelayCounter = 0; 
+
+  input = (((DelayBuffer[DelayCounter] << 8) | ADC_low) + 0x8000)+(((ADC_high << 8) | ADC_low) + 0x8000); // make a signed 16b value
+  
   //write the PWM signal
   OCR1AL = ((input + 0x8000) >> 8); // convert to unsigned, send out high byte
   OCR1BL = input; // send out low byte
+
 }
